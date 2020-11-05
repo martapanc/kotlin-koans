940f056fc393aa6b1911343b2957465532358e76
Marta Pancaldi <marta.panc@gmail.com>
2020-11-05 19:20:34 +0000

    Add Class and Generics example

---
+++ src/main/kotlin/Class.kt
@@ -0,0 +1,13 @@
+
+class Customer
+
+class Contact(val id: Int, var email: String)
+
+fun main() {
+    val customer = Customer()
+    val contact = Contact(1, "test@gmail.com")
+
+    println(customer)
+    println(contact.id)
+    println(contact.email)
+}
---
+++ src/main/kotlin/Generics.kt
@@ -0,0 +1,39 @@
+
+// https://kotlinlang.org/docs/reference/generics.html
+class MutableStack<E>(vararg items: E) {
+    private val elements = items.toMutableList()
+
+    fun push(element: E) = elements.add(element)
+
+    fun peek(): E = elements.last()
+
+    fun pop(): E = elements.removeAt(elements.size - 1)
+
+    fun isEmpty() = elements.isEmpty()
+
+    fun size() = elements.size
+
+    override fun toString(): String {
+        return "MutableStack(${elements.joinToString()})"
+    }
+}
+
+fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements)
+
+fun main() {
+    val mutableStack = MutableStack(1, 2, 15, 28)
+
+    println(mutableStack.size())
+    println(mutableStack.peek())
+    println(mutableStack.push(57))
+    println(mutableStack.size())
+    println(mutableStack.pop())
+    println(mutableStack.isEmpty())
+    println(mutableStack)
+
+    val stack = mutableStackOf(0.5, 12.69, 2)
+    println(stack)
+    println(stack.push(16.1))
+    println(stack.size())
+    println(stack)
+}
