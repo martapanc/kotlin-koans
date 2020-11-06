d4284582e62080fe3de6f2bae09dc466879dbb1d
Marta Pancaldi <marta.panc@gmail.com>
2020-11-06 17:39:06 +0000

    Add When and Loops

---
+++ src/main/kotlin/controlFlow/Loops.kt
@@ -0,0 +1,45 @@
+package controlFlow
+
+val cakes = listOf("Carrot", "Cheese", "Chocolate")
+
+fun main() {
+
+    for (cake in cakes) {
+        println("Yummy, it's a $cake cake")
+    }
+
+    var cakesEaten = 0
+    var cakesBaked = 0
+    while (cakesEaten < 5) {
+        eatACake()
+        cakesEaten++
+    }
+
+    do {
+        bakeACake()
+        cakesBaked++
+    } while (cakesBaked < cakesEaten)
+
+
+    val zoo = Zoo(listOf(Animal("zebra"), Animal("lion")))
+    for (animal in zoo) {
+        println("Watch out, there's a ${animal.name} crossing")
+    }
+}
+
+fun eatACake() = println("Eat a Cake")
+fun bakeACake() = println("Bake a Cake")
+
+
+class Animal(val name: String)
+class Zoo(val animals: List<Animal>) {
+    operator fun iterator(): Iterator<Animal> {
+        return animals.iterator()
+    }
+}
+
+// Defines an iterator in a class. It must be named iterator and have the operator modifier.
+// Returns the iterator that meets the following method requirements:
+// next(): Animal
+// hasNext(): Boolean
+// Loops through animals in the zoo with the user-defined iterator.
---
+++ src/main/kotlin/controlFlow/When.kt
@@ -0,0 +1,41 @@
+package controlFlow
+
+import intro.Customer
+
+
+fun main() {
+    cases("Hello")
+    cases("1")
+    cases(2)
+    cases(2L)
+    cases(Customer())
+    cases(MyClass())
+    cases("hello")
+
+    println(whenAssign(1))
+    println(whenAssign("Hello"))
+    println(whenAssign(4L))
+    println(whenAssign(MyClass()))
+}
+
+fun cases(obj: Any) {
+    print("${obj}: ")
+    when (obj) {
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long")
+        !is String -> println("Not a string")
+        else -> println("Unknown")
+    }
+}
+
+fun whenAssign(obj: Any): Any {
+    return when(obj) {
+        1 -> "one"
+        "Hello" -> 1
+        is Long -> false
+        else -> 42
+    }
+}
+
+class MyClass
