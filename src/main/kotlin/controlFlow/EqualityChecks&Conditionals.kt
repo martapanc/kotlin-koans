0a967834982ba375aa82a4903c5230368a48d455
Marta Pancaldi <marta.panc@gmail.com>
2020-11-08 17:11:01 +0000

    Add EqualityChecks and COnditional Expression

---
+++ src/main/kotlin/controlFlow/EqualityChecks&Conditionals.kt
@@ -0,0 +1,16 @@
+package controlFlow
+
+val authors = setOf("Shakespeare", "Hemingway", "Twain")
+val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
+fun main() {
+    println(authors == writers)
+    println(authors === writers)
+
+    println(max(2, 8))
+}
+
+//1 Returns true because it calls authors.equals(writers) and sets ignore element order.
+//2 Returns false because authors and writers are distinct references.
+
+fun max(a: Int, b: Int) = if (a > b) a else b
