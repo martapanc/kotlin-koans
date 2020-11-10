7069895124142f67e6d0dffc8db1811d1dd934a1
Marta Pancaldi <marta.panc@gmail.com>
2020-11-10 20:18:16 +0000

    Add List, Set and Map examples

---
+++ src/main/kotlin/collections/ListSet&Map.kt
@@ -0,0 +1,65 @@
+package collections
+val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3)
+val sudoers: List<Int> = systemUsers
+
+fun addSudoer(newUser: Int) {
+    systemUsers.add(newUser)
+}
+
+fun getSysSudoers(): List<Int> {
+    return sudoers
+}
+
+
+val openIssues: MutableSet<String> = mutableSetOf("uniqueDescr1", "uniqueDescr2", "uniqueDescr3")
+
+fun addIssue(uniqueDesc: String): Boolean {
+    return openIssues.add(uniqueDesc)
+}
+
+fun getStatusLog(isAdded: Boolean): String {
+    return if (isAdded) "registered correctly." else "marked as duplicate and rejected."
+}
+
+
+const val POINTS_X_PASS: Int = 15
+val EZPassAccounts: MutableMap<Int, Int> = mutableMapOf(1 to 100, 2 to 100, 3 to 100)
+val EZPassReport: Map<Int, Int> = EZPassAccounts
+
+fun updatePointsCredit(accountId: Int) {
+    if (EZPassAccounts.containsKey(accountId)) {
+        println("Updating $accountId...")
+        EZPassAccounts[accountId] = EZPassAccounts.getValue(accountId) + POINTS_X_PASS
+    } else {
+        println("Error: Trying to update a non-existing account (id: $accountId)")
+    }
+}
+
+fun accountsReport() {
+    println("EZ-Pass report:")
+    EZPassReport.forEach {
+            k, v -> println("ID $k: credit $v")
+    }
+}
+
+fun main() {
+    addSudoer(4)
+    println("Tot collections.getSudoers: ${getSysSudoers().size}")
+    getSysSudoers().forEach { i ->
+        println("Some useful info on user $i")
+    }
+    // collections.getSysSudoers().add(5) <- Error!
+
+
+    val aNewIssue: String = "uniqueDescr4"
+    val anIssueAlreadyIn: String = "uniqueDescr2"
+    println("Issue $aNewIssue ${getStatusLog(addIssue(aNewIssue))}")
+    println("Issue $anIssueAlreadyIn ${getStatusLog(addIssue(anIssueAlreadyIn))}")
+
+
+    accountsReport()
+    updatePointsCredit(1)
+    updatePointsCredit(1)
+    updatePointsCredit(5)
+    accountsReport()
+}
