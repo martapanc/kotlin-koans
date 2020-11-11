035920f96ebcd98184981102c683cf73504102bc
Marta Pancaldi <marta.panc@gmail.com>
2020-11-11 16:15:51 +0000

    Add FlatMap, Min & Max, Sortedg

---
+++ src/main/kotlin/collections/FlatmapMinMaxSorted.kt
@@ -0,0 +1,25 @@
+package collections
+
+import kotlin.math.abs
+
+val numbers3 = listOf(1, 2, 3)
+
+val tripled3 = numbers.flatMap { listOf(it, it, it) }
+val empty3 = emptyList<Int>()
+
+val shuffled = listOf(5, 4, 2, 1, 3, -10)
+val natural = shuffled.sorted()
+val inverted = shuffled.sortedBy { -it }
+val descending = shuffled.sortedDescending()
+val descendingBy = shuffled.sortedByDescending { abs(it)  }
+
+fun main() {
+    println(tripled3)
+    println("Numbers: $numbers3, min = ${numbers3.min()} max = ${numbers3.max()}") // 1
+    println("Empty: $empty3, min = ${empty3.min()}, max = ${empty3.max()}")        // 2
+
+    println(natural)
+    println(inverted)
+    println(descending)
+    println(descendingBy)
+}
