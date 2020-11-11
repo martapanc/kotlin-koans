29c66f6b770ab9ff5195d8f0f61471d95109c622
Marta Pancaldi <marta.panc@gmail.com>
2020-11-11 14:03:04 +0000

    Add more examples

--- src/main/kotlin/collections/AssociateGroupBy.kt
+++ src/main/kotlin/collections/AssociateGroupBy.kt
@@ -9,8 +9,16 @@ val people = listOf(
     Person("Vasilisa", "Saint-Petersburg", "+7-999-123456"),
     Person("John", "London", "+7-999-456789"))
 
+// The difference between associateBy and groupBy is how they process objects with the same key:
+// - associateBy uses the last suitable element as the value.
+// - groupBy builds a list of all suitable elements and puts it in the value.
+//
+// associateBy(phone, city): {+1-888-123456=Boston, +49-777-789123=Munich, +7-999-456789=London, +7-999-123456=Saint-Petersburg}
+// groupBy(phone, city):     {+1-888-123456=[Boston], +49-777-789123=[Munich], +7-999-456789=[Saint-Petersburg, London], +7-999-123456=[Saint-Petersburg]}
+
 val phoneBook = people.associateBy { it.phone }
 val cityBook = people.associateBy(Person::phone, Person::city)
+val cityBook2 = people.groupBy(Person::phone, Person::city)
 val peopleCities = people.groupBy(Person::city, Person::name)
 val cityPeople = people.groupBy(Person::name, Person::city)
 val phonePeople = people.groupBy(Person::name, Person::phone)
@@ -19,6 +27,7 @@ val peoplePhone = people.groupBy(Person::phone, Person::name)
 fun main() {
     println(phoneBook)
     println(cityBook)
+    println(cityBook2)
     println(peopleCities)
     println(cityPeople)
     println(phonePeople)
