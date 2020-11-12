69d545826d0902e232b19d1b69450f8d2eb96ca9
Marta Pancaldi <marta.panc@gmail.com>
2020-11-12 18:08:04 +0000

    Add Apply and Also examples

---
+++ src/main/kotlin/scopeFunctions/ApplyAlso.kt
@@ -0,0 +1,26 @@
+package scopeFunctions
+
+data class Person(var name: String, var age: Int, var about: String) {
+    constructor() : this("", 0, "")
+}
+
+val jake = Person()
+val stringDescription = jake.apply {
+    name = "Jake"
+    age = 30
+    about = "Android developer"
+}.toString()
+
+val otherJake = Person("Jake", 30, "iOS Developer")
+    .also {
+        writeCreationLog(it)
+    }
+
+fun main() {
+    println(stringDescription)
+    println(otherJake)
+}
+
+fun writeCreationLog(p: Person) {
+    println("A new person ${p.name} was created.")
+}
