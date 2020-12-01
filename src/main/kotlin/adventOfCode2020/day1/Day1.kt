dfbe609677c4e37d1d326400455fc6d4de90afc2
Marta Pancaldi <marta.panc@gmail.com>
2020-12-01 10:51:34 +0000

    Add AoC 2020 D01P2

--- src/main/kotlin/adventOfCode2020/day1/Day1.kt
+++ src/main/kotlin/adventOfCode2020/day1/Day1.kt
@@ -25,8 +25,38 @@ fun findPairAndMultiply(inputMap: Map<Int, Int>) : Int {
         if (inputMap.containsKey(input.value)) {
             first = input.key
             second = input.value
+            break
         }
     }
 
     return first * second
+}
+
+fun findTripletAndMultiply(inputMap: Map<Int, Int>) : Int {
+    var first = 0
+    var second = 0
+    var third = 0
+
+    for (input1 in inputMap) {
+        val diff = input1.value
+
+        for (input2 in inputMap) {
+            val candidateSecond = input2.key
+            if (candidateSecond < diff) {
+                val candidateThird = diff - candidateSecond
+                if (inputMap.containsKey(candidateThird)) {
+                    first = input1.key
+                    second = candidateSecond
+                    third = candidateThird
+                    break
+                }
+            }
+        }
+
+        if (first != 0) {
+            break
+        }
+    }
+
+    return first * second * third
 }
--- src/test/kotlin/adventOfCode2020/day1/Day1KtTest.kt
+++ src/test/kotlin/adventOfCode2020/day1/Day1KtTest.kt
@@ -6,17 +6,23 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class Day1KtTest {
 
-    private val testInput = "src/main/kotlin/adventOfCode2020/day1/input0"
-    private val input = "src/main/kotlin/adventOfCode2020/day1/input"
+    private val testInputMap = readInputFileToMap("src/main/kotlin/adventOfCode2020/day1/input0")
+    private val inputMap = readInputFileToMap("src/main/kotlin/adventOfCode2020/day1/input")
 
     @Test
     fun testReadInputFileToMap() {
-        println(readInputFileToMap(input))
+        println(inputMap)
     }
 
     @Test
     fun testFindPairAndMultiply() {
-        assertEquals(514579, findPairAndMultiply(readInputFileToMap(testInput)))
-        assertEquals(972576, findPairAndMultiply(readInputFileToMap(input)))
+        assertEquals(514579, findPairAndMultiply(testInputMap))
+        assertEquals(972576, findPairAndMultiply(inputMap))
+    }
+
+    @Test
+    fun testFindTripletAndMultiply() {
+        assertEquals(241861950, findTripletAndMultiply(testInputMap))
+        assertEquals(199300880, findTripletAndMultiply(inputMap))
     }
 }
