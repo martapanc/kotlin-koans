a1a6ec4e812e1ba06fcc17b91f5f5b211632430f
Marta Pancaldi <marta.panc@gmail.com>
2020-12-06 18:11:20 +0000

    Aoc2020 D6P2 - refactor

--- src/main/kotlin/adventOfCode2020/day6/Day6.kt
+++ src/main/kotlin/adventOfCode2020/day6/Day6.kt
@@ -1,26 +1,5 @@
 package adventOfCode2020.day6
 
-import java.io.File
-import java.io.InputStream
-
-fun readInput(path: String) : List<String> {
-    val inputStream: InputStream = File(path).inputStream()
-    val lineList = mutableListOf<String>()
-    inputStream.bufferedReader().forEachLine { lineList.add(it) }
-
-    val answersList = mutableListOf<String>()
-    var answers = ""
-    for (line in lineList) {
-        if (line.isNotEmpty()) {
-            answers += if (answers.isBlank()) { line } else { " $line" }
-        } else {
-            answersList.add(answers)
-            answers = ""
-        }
-    }
-    return answersList;
-}
-
 fun countUniqueLetters(answers: String): Int {
     val set = mutableSetOf<Char>()
     for (answer in answers.split(" ")) {
@@ -31,10 +10,6 @@ fun countUniqueLetters(answers: String): Int {
     return set.size
 }
 
-fun countTotalAnswers(list: List<String>): Int {
-    return list.sumBy { countUniqueLetters(it) }
-}
-
 fun countCommonLetters(answersString: String): Int {
     val answerGroupList = answersString.split(" ")
     if (answerGroupList.size == 1) {
@@ -42,24 +17,20 @@ fun countCommonLetters(answersString: String): Int {
     }
 
     val setOfLetters = mutableSetOf<Char>()
-    val setOfLettersCopy = mutableSetOf<Char>()
     for (answers in answerGroupList) {
-        for (char in answers) {
-            setOfLetters.add(char)
-            setOfLettersCopy.add(char)
-        }
+        setOfLetters.addAll(answers.toList())
     }
 
+    val setOfLettersCopy = setOfLetters.toMutableList()
     for (answer in answerGroupList) {
-        for (char in setOfLetters) {
-            if (!answer.contains(char)) {
-                setOfLettersCopy.remove(char)
-            }
-        }
+        setOfLetters
+            .asSequence()
+            .filterNot { answer.contains(it) }
+            .forEach { setOfLettersCopy.remove(it) }
     }
     return setOfLettersCopy.size
 }
 
-fun countTotalCommonAnswers(list: List<String>): Int {
-    return list.sumBy { countCommonLetters(it) }
+fun countTotalAnswers(list: List<String>, countMethod: (String) -> Int): Int {
+    return list.sumBy { countMethod(it) }
 }
--- src/test/kotlin/adventOfCode2020/day6/Day6KtTest.kt
+++ src/test/kotlin/adventOfCode2020/day6/Day6KtTest.kt
@@ -1,13 +1,14 @@
 package adventOfCode2020.day6
 
+import adventOfCode2020.day4.readInputFile
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
 internal class Day6KtTest {
 
-    private val input = readInput("src/main/kotlin/adventOfCode2020/day6/input")
-    private val input0 = readInput("src/main/kotlin/adventOfCode2020/day6/input0")
+    private val input = readInputFile("src/main/kotlin/adventOfCode2020/day6/input")
+    private val input0 = readInputFile("src/main/kotlin/adventOfCode2020/day6/input0")
 
     @Test
     fun readInput() {
@@ -25,21 +26,21 @@ internal class Day6KtTest {
 
     @Test
     fun testCountCommonLetters() {
-        assertEquals(3, countCommonLetters("abc"))
         assertEquals(0, countCommonLetters("i x i i"))
         assertEquals(1, countCommonLetters("ab ac"))
+        assertEquals(3, countCommonLetters("abc"))
         assertEquals(3, countCommonLetters("abcx abcy abcz"))
     }
 
     @Test
     fun testCountTotalAnswers() {
-        assertEquals(17, countTotalAnswers(input0))
-        assertEquals(17, countTotalAnswers(input))
+        assertEquals(17, countTotalAnswers(input0, ::countUniqueLetters))
+        assertEquals(6768, countTotalAnswers(input, ::countUniqueLetters))
     }
 
     @Test
     fun testCountCommonAnswers() {
-        assertEquals(9, countTotalCommonAnswers(input0))
-        assertEquals(17, countTotalCommonAnswers(input))
+        assertEquals(9, countTotalAnswers(input0, ::countCommonLetters))
+        assertEquals(3489, countTotalAnswers(input, ::countCommonLetters))
     }
 }
