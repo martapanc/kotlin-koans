3a86a4a233678c78d9afca92d20d765243f87f8b
Marta Pancaldi <marta.panc@gmail.com>
2020-12-08 09:23:44 +0000

    AoC2020 D8P1 - complete

---
+++ src/main/kotlin/adventOfCode2020/day8/Day8.kt
@@ -0,0 +1,46 @@
+package adventOfCode2020.day8
+
+import java.io.File
+import java.io.InputStream
+
+fun readInputToMap(path: String): Map<Int, GameData> {
+    val inputStream: InputStream = File(path).inputStream()
+    val lineList = mutableListOf<String>()
+    inputStream.bufferedReader().forEachLine { lineList.add(it) }
+
+    val inputMap = mutableMapOf<Int, GameData>()
+    for ((i, line) in lineList.withIndex()) {
+       val data = line.split(" ")
+        inputMap[i] = GameData(data[0], data[1].toInt(), false)
+    }
+    return inputMap
+}
+
+fun playTheGame(inputMap: Map<Int, GameData>): Int {
+    var accumulator = 0
+    var index = 0
+    while (true) {
+        val current: GameData? = inputMap[index]
+        if (current!!.executed) {
+            break
+        }
+        when (current.command) {
+            "nop" -> {
+                index++
+            }
+            "acc" -> {
+                accumulator += current.amount
+                index++
+            }
+            "jmp" -> {
+                index += current.amount
+            }
+            else -> println("error")
+        }
+        current.executed = true
+    }
+
+    return accumulator
+}
+
+data class GameData(var command: String, var amount: Int, var executed: Boolean) {}
