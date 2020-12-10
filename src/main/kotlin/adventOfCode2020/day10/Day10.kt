b19cb20c8cb3a8af4a1d1536d7679035e5227fbe
Marta Pancaldi <marta.panc@gmail.com>
2020-12-10 12:09:45 +0000

    AoC2020 D10P2: WIP

--- src/main/kotlin/adventOfCode2020/day10/Day10.kt
+++ src/main/kotlin/adventOfCode2020/day10/Day10.kt
@@ -22,3 +22,38 @@ fun findJoltsDifferences(list: List<Int>): Int {
     }
     return diffOf1Count * diffOf3Count
 }
+
+// (1<<j) is a number with jth bit 1 so when we 'and' them with the subset number we get which numbers are present in the subset and which are not
+fun generateSubsets(list: List<Int>): List<List<Int>> {
+    return (0 until (1 shl list.size)).map { i ->
+        (list.indices)
+            .filter { i and (1 shl it) > 0 }
+            .map { list[it] }
+    }
+}
+
+fun generateValidSubsets(list: List<Int>): List<List<Int>> {
+    val validSubsets = mutableListOf<List<Int>>()
+    val subsetsList = generateSubsets(list)
+    val min = list[0] - 1
+    val max = list[list.size - 1] + 1
+
+    for (subset in subsetsList) {
+        val extendedSubset = subset.toMutableList()
+        extendedSubset.add(0, min)
+        extendedSubset.add(max)
+        if (isMaxDifferenceLessThan3(extendedSubset)) {
+            validSubsets.add(subset)
+        }
+    }
+    return validSubsets
+}
+
+fun getNumberOfValidSubsets(list: List<Int>): Int {
+    return generateValidSubsets(list).size
+}
+
+private fun isMaxDifferenceLessThan3(extendedSubset: MutableList<Int>): Boolean {
+    return (0 until extendedSubset.size - 1)
+        .none { extendedSubset[it + 1] - extendedSubset[it] > 3 }
+}
--- src/test/kotlin/adventOfCode2020/day10/Day10KtTest.kt
+++ src/test/kotlin/adventOfCode2020/day10/Day10KtTest.kt
@@ -18,8 +18,27 @@ internal class Day10KtTest {
 
     @Test
     fun findJoltsDifferences() {
-        assertEquals(35, findJoltsDifferences(input0))
+//        assertEquals(35, findJoltsDifferences(input0))
         assertEquals(220, findJoltsDifferences(input1))
         assertEquals(1984, findJoltsDifferences(input))
+    }
+
+    @Test
+    fun printSubsets() {
+        assertEquals(listOf(listOf<Int>()), generateSubsets(mutableListOf()))
+        assertEquals(listOf(listOf(), listOf(1)), generateSubsets(mutableListOf(1)))
+        assertEquals(4, generateSubsets(mutableListOf(1, 2)).size)
+        assertEquals(8, generateSubsets(mutableListOf(1, 2, 3)).size)
+        println(generateSubsets(mutableListOf(1, 2, 3, 4)))
+    }
+
+    @Test
+    fun generateValidSubsets() {
+        assertEquals(7, getNumberOfValidSubsets(listOf(1, 2, 3)))
+        assertEquals(7, generateValidSubsets((listOf(4, 5, 6))).size)
+        assertEquals(13, generateValidSubsets((listOf(4, 5, 6, 7))).size)
+        assertEquals(24, generateValidSubsets((listOf(4, 5, 6, 7, 8))).size)
+        assertEquals(44, generateValidSubsets((listOf(4, 5, 6, 7, 8, 9))).size)
+        assertEquals(81, generateValidSubsets((listOf(4, 5, 6, 7, 8, 9, 10))).size)
     }
 }
