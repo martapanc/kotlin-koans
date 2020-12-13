1eaae885a7ef8e8d3778dba8b4a13fe6d0d8f46b
Marta Pancaldi <marta.panc@gmail.com>
2020-12-13 21:13:27 +0000

    AoC2020 D12: refactor

--- src/main/kotlin/adventOfCode2020/day12/Day12.kt
+++ src/main/kotlin/adventOfCode2020/day12/Day12.kt
@@ -10,21 +10,21 @@ fun readInputToList(path: String): List<Instruction> {
 }
 
 fun navigateShip(instructions: List<Instruction>): Int {
-    var position = CC(0, 0)
+    var position = Coordinate(0, 0)
     var facingDirection = CardinalPoints.EAST
-    for (instruct in instructions) {
-        when (instruct.direction) {
-            'N' -> position = CC(position.x, position.y + instruct.value)
-            'S' -> position = CC(position.x, position.y - instruct.value)
-            'E' -> position = CC(position.x + instruct.value, position.y)
-            'W' -> position = CC(position.x - instruct.value, position.y)
-            'L', 'R' -> facingDirection = rotateShip(facingDirection, instruct.value, instruct.direction)
+    for (instruction in instructions) {
+        when (instruction.direction) {
+            'N' -> position = Coordinate(position.x, position.y + instruction.value)
+            'S' -> position = Coordinate(position.x, position.y - instruction.value)
+            'E' -> position = Coordinate(position.x + instruction.value, position.y)
+            'W' -> position = Coordinate(position.x - instruction.value, position.y)
+            'L', 'R' -> facingDirection = rotateShip(facingDirection, instruction.value, instruction.direction)
             'F' -> {
                 position = when (facingDirection) {
-                    CardinalPoints.EAST -> CC(position.x + instruct.value, position.y)
-                    CardinalPoints.SOUTH -> CC(position.x, position.y - instruct.value)
-                    CardinalPoints.WEST -> CC(position.x - instruct.value, position.y)
-                    CardinalPoints.NORTH -> CC(position.x, position.y + instruct.value)
+                    CardinalPoints.EAST -> Coordinate(position.x + instruction.value, position.y)
+                    CardinalPoints.SOUTH -> Coordinate(position.x, position.y - instruction.value)
+                    CardinalPoints.WEST -> Coordinate(position.x - instruction.value, position.y)
+                    CardinalPoints.NORTH -> Coordinate(position.x, position.y + instruction.value)
                 }
             }
         }
@@ -46,40 +46,40 @@ fun rotateShip(facingDirection: CardinalPoints, degrees: Int, direction: Char): 
     rotationAmount = if (direction == 'R') rotationAmount else -rotationAmount
     var newDirectionIndex = clockwise.indexOf(facingDirection) + rotationAmount
     // Sadly, Kotlin doesn't support negative indices for lists...
-    newDirectionIndex = if (newDirectionIndex < 0) 4 + newDirectionIndex else newDirectionIndex % 4
+    newDirectionIndex = if (newDirectionIndex < 0) 4 - abs(newDirectionIndex) else newDirectionIndex % 4
     return clockwise[newDirectionIndex]
 }
 
 fun navigateShipAndWaypoint(instructions: List<Instruction>): Int {
-    var shipPos = CC(0, 0)
-    var wpPos = CC(10, 1)
+    var shipPos = Coordinate(0, 0)
+    var waypointPos = Coordinate(10, 1)
 
     for (instruction in instructions) {
-        val relativeWpPos = CC(wpPos.x - shipPos.x, wpPos.y - shipPos.y)
+        val relativeWpPos = Coordinate(waypointPos.x - shipPos.x, waypointPos.y - shipPos.y)
         when (instruction.direction) {
-            'N' -> wpPos = CC(wpPos.x, wpPos.y + instruction.value)
-            'S' -> wpPos = CC(wpPos.x, wpPos.y - instruction.value)
-            'E' -> wpPos = CC(wpPos.x + instruction.value, wpPos.y)
-            'W' -> wpPos = CC(wpPos.x - instruction.value, wpPos.y)
+            'N' -> waypointPos = Coordinate(waypointPos.x, waypointPos.y + instruction.value)
+            'S' -> waypointPos = Coordinate(waypointPos.x, waypointPos.y - instruction.value)
+            'E' -> waypointPos = Coordinate(waypointPos.x + instruction.value, waypointPos.y)
+            'W' -> waypointPos = Coordinate(waypointPos.x - instruction.value, waypointPos.y)
             'L' -> {
-                wpPos = when (instruction.value) {
-                    90 -> CC(shipPos.x - relativeWpPos.y, shipPos.y + relativeWpPos.x)
-                    180 -> CC(shipPos.x - relativeWpPos.x, shipPos.y - relativeWpPos.y)
-                    else -> CC(shipPos.x + relativeWpPos.y, shipPos.y - relativeWpPos.x)
+                waypointPos = when (instruction.value) {
+                    90 -> Coordinate(shipPos.x - relativeWpPos.y, shipPos.y + relativeWpPos.x)
+                    180 -> Coordinate(shipPos.x - relativeWpPos.x, shipPos.y - relativeWpPos.y)
+                    else -> Coordinate(shipPos.x + relativeWpPos.y, shipPos.y - relativeWpPos.x)
                 }
             }
             'R' -> {
-                wpPos = when (instruction.value) {
-                    90 -> CC(shipPos.x + relativeWpPos.y, shipPos.y - relativeWpPos.x)
-                    180 -> CC(shipPos.x - relativeWpPos.x, shipPos.y - relativeWpPos.y)
-                    else -> CC(shipPos.x - relativeWpPos.y, shipPos.y + relativeWpPos.x)
+                waypointPos = when (instruction.value) {
+                    90 -> Coordinate(shipPos.x + relativeWpPos.y, shipPos.y - relativeWpPos.x)
+                    180 -> Coordinate(shipPos.x - relativeWpPos.x, shipPos.y - relativeWpPos.y)
+                    else -> Coordinate(shipPos.x - relativeWpPos.y, shipPos.y + relativeWpPos.x)
                 }
             }
             'F' -> {
                 val newShipX = shipPos.x + relativeWpPos.x * instruction.value
                 val newShipY = shipPos.y + relativeWpPos.y * instruction.value
-                shipPos = CC(newShipX, newShipY)
-                wpPos = CC(shipPos.x + relativeWpPos.x, shipPos.y + relativeWpPos.y)
+                shipPos = Coordinate(newShipX, newShipY)
+                waypointPos = Coordinate(shipPos.x + relativeWpPos.x, shipPos.y + relativeWpPos.y)
             }
         }
     }
@@ -90,4 +90,4 @@ data class Instruction(var direction: Char, var value: Int)
 
 enum class CardinalPoints { NORTH, SOUTH, EAST, WEST }
 
-class CC(var x: Int, var y: Int)
+class Coordinate(var x: Int, var y: Int)
