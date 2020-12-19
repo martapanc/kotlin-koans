99ec1bd0a00127813aa5a3021b68904164046092
Marta Pancaldi <marta.panc@gmail.com>
2020-12-19 15:04:26 +0000

    AoC2020 D19D2: complete

--- src/main/kotlin/adventOfCode2020/day19/Day19.kt
+++ src/main/kotlin/adventOfCode2020/day19/Day19.kt
@@ -2,6 +2,9 @@ package adventOfCode2020.day19
 
 import java.io.File
 
+// The required depth is 13 for the large input - setting it to 20 as it doesn't affect the performance significantly
+var depthLimit = 20
+
 fun readRulesToMap(path: String): Map<String, String> {
     val lineList = mutableListOf<String>()
     File(path).inputStream().bufferedReader().forEachLine { lineList.add(it) }
@@ -18,20 +21,23 @@ fun readMessagesToList(path: String): List<String> {
     return lineList
 }
 
-fun findRegex(map: Map<String, String>): String {
-    return recursivelyReplace("0", map)
+fun findRegex(ruleMap: Map<String, String>): String {
+    return recursivelyReplace("0", ruleMap, 0, false)
 }
 
-fun recursivelyReplace(rule: String, map: Map<String, String>): String {
+fun recursivelyReplace(rule: String, map: Map<String, String>, depth: Int, limitDepth: Boolean): String {
     val ruleValue = map[rule]
     if (ruleValue == "a" || ruleValue == "b") {
         return ruleValue
+    }
+    if (limitDepth && depth > depthLimit) {
+        return ""
     }
     var regex = if (ruleValue!!.contains("|")) "(" else ""
     for ((index, split) in ruleValue.split("|").withIndex()) {
         val rules = split.trim().split(" ")
         for (r in rules) {
-            val recursivelyReplace = recursivelyReplace(r, map)
+            val recursivelyReplace = recursivelyReplace(r, map, depth + 1, limitDepth)
             regex = "$regex$recursivelyReplace"
         }
         if (ruleValue.contains("|") && index == 0) {
@@ -44,4 +50,27 @@ fun recursivelyReplace(rule: String, map: Map<String, String>): String {
 
 fun countValidMessages(messages: List<String>, regexString: String): Int {
     return messages.count { it.matches(Regex(regexString)) }
+}
+
+// To avoid infinite loops, limit the depth an infinite rule can reach
+fun countValidMessagesV2(messages: List<String>, ruleMap: Map<String, String>): Int {
+    val regex = recursivelyReplace("0", ruleMap, 0, true)
+    return messages.count { it.matches(Regex(regex)) }
+}
+
+// All rules but 0, 8, 11 are finite
+fun testsWithNewRules(rules: Map<String, String>) {
+    val listOfRulesThatFinish = mutableListOf<String>()
+    val sortedRules = rules.toSortedMap(compareBy<String> { it.length }.thenBy { it })
+    for (rule in sortedRules.keys) {
+        print("Rule $rule started at " + System.currentTimeMillis())
+        try {
+            println(" : " + recursivelyReplace(rule, rules, 0, false))
+            listOfRulesThatFinish.add(rule)
+        } catch (error: StackOverflowError) {
+            println(" : stack overflow")
+            continue
+        }
+    }
+    println(listOfRulesThatFinish)
 }
--- src/test/kotlin/adventOfCode2020/day19/Day19KtTest.kt
+++ src/test/kotlin/adventOfCode2020/day19/Day19KtTest.kt
@@ -25,7 +25,8 @@ internal class Day19KtTest {
     @Test
     fun testFindRegex() {
         assertEquals("a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b", findRegex(input0Rules))
-        assertEquals("(a(b(((b(bba|a(ba|bb))|a(a(aa|bb)|b((b|a)a|ab)))b|(a(aba|(b(b|a)|ab)b)|b(b(b(b|a)|ab)|a((b|a)a|ab)))a)a|(b(a((ba|ab)a|(aa|bb)b)|b((ab|bb)b|(b(b|a)|ab)a))|a(a(b(a(b|a)|bb)|aab)|b(baa|a((b|a)a|ab))))b)|a(a((bbaa|a((ba|bb)b|(ba|aa)a))a|(baab|a(baa|a((b|a)a|ab)))b)|b((a(a(a(b|a)|bb)|b(ba|aa))|b((ba|aa)a|abb))a|((a(ab|aa)|b(ba|aa))b|(a(aa|bb)|b((b|a)a|ab))a)b)))|b(((b((aba|b(aa|bb))a|(((b|a)a|ab)b|(a(b|a)|bb)a)b)|a(a(baa|(ba|bb)b)|b(b(b|a)(b|a)|a(b(b|a)|ab))))a|((b(b(b|a)(b|a)|a(b(b|a)|ab))|a(b((b|a)a|ab)|aaa))b|(a((ba|aa)a|abb)|b((ab|aa)b|baa))a)b)a|((b((aba|abb)a|(b(ba|bb)|aaa)b)|a(b(b(b|a)(b|a)|a(b(b|a)|ab))|a(aab|b(ab|bb))))a|(a(b((ab|aa)a|(ba|ab)b)|a(b((b|a)a|ab)|aab))|b(b(b(ba|bb)|aaa)|a(b|a)(ba|ab)))b)b))(a(b(((b(bba|a(ba|bb))|a(a(aa|bb)|b((b|a)a|ab)))b|(a(aba|(b(b|a)|ab)b)|b(b(b(b|a)|ab)|a((b|a)a|ab)))a)a|(b(a((ba|ab)a|(aa|bb)b)|b((ab|bb)b|(b(b|a)|ab)a))|a(a(b(a(b|a)|bb)|aab)|b(baa|a((b|a)a|ab))))b)|a(a((bbaa|a((ba|bb)b|(ba|aa)a))a|(baab|a(baa|a((b|a)a|ab)))b)|b((a(a(a(b|a)|bb)|b(ba|aa))|b((ba|aa)a|abb))a|((a(ab|aa)|b(ba|aa))b|(a(aa|bb)|b((b|a)a|ab))a)b)))|b(((b((aba|b(aa|bb))a|(((b|a)a|ab)b|(a(b|a)|bb)a)b)|a(a(baa|(ba|bb)b)|b(b(b|a)(b|a)|a(b(b|a)|ab))))a|((b(b(b|a)(b|a)|a(b(b|a)|ab))|a(b((b|a)a|ab)|aaa))b|(a((ba|aa)a|abb)|b((ab|aa)b|baa))a)b)a|((b((aba|abb)a|(b(ba|bb)|aaa)b)|a(b(b(b|a)(b|a)|a(b(b|a)|ab))|a(aab|b(ab|bb))))a|(a(b((ab|aa)a|(ba|ab)b)|a(b((b|a)a|ab)|aab))|b(b(b(ba|bb)|aaa)|a(b|a)(ba|ab)))b)b))(b(a(((b((aa|bb)b|(ba|bb)a)|a((ab|bb)b|(ab|aa)a))b|((a(b|a)(b|a)|baa)a|aabb)a)b|((((a(b|a)|bb)b|aba)b|(b(b(b|a)|ab)|a(ba|ab))a)b|(((ba|aa)a|(ab|aa)b)a|(aab|aaa)b)a)a)|b(((a((ab|bb)b|(ba|aa)a)|b(a(b(b|a)|ab)|b(ab|aa)))b|(a((b(b|a)|ab)b|(ab|aa)a)|b((b|a)(b|a)a|(ab|aa)b))a)b|((b(aba|b(ba|aa))|a((ba|aa)a|aab))b|(b((ba|ab)a|(aa|bb)b)|a(bba|a(ba|aa)))a)a))|a(a(a((b(b(b(b|a)|ab)|a(b|a)(b|a))|a((ab|bb)a|(ab|aa)b))a|(((ba|bb)a|(b(b|a)|ab)b)a|((ab|bb)a|(b|a)(b|a)b)b)b)|b((((ab|bb)b|(b(b|a)|ab)a)b|(aab|b(ab|bb))a)a|((aba|(b(b|a)|ab)b)b|(aba|(aa|bb)b)a)b))|b(b(a(b(baa|(ba|ab)b)|a((aa|bb)b|(ab|bb)a))|b((bab|a(ab|aa))b|(aaa|(a(b|a)|bb)b)a))|a(((aba|(aa|bb)b)b|(a(ab|aa)|b(ba|aa))a)a|((aab|b(b(b|a)|ab))b|(aaa|(a(b|a)|bb)b)a)b))))", findRegex(inputRules))
+        assertEquals("(a(b(((b(bba|a(ba|bb))|a(a(aa|bb)|b((b|a", findRegex(inputRules).substring(0, 40))
+        assertEquals("b(b(b|a)|ab))b|(aaa|(a(b|a)|bb)b)a)b))))", findRegex(inputRules).substring(2115, 2155))
     }
 
     @Test
@@ -33,5 +34,16 @@ internal class Day19KtTest {
         assertEquals(2, countValidMessages(input0Messages, findRegex(input0Rules)))
         assertEquals(3, countValidMessages(input2Messages, findRegex(input2Rules)))
         assertEquals(132, countValidMessages(inputMessages, findRegex(inputRules)))
+    }
+
+    @Test
+    fun testNewRules() {
+        testsWithNewRules(input2RulesV2)
+    }
+
+    @Test
+    fun testCountValidMessagesV2() {
+        assertEquals(12, countValidMessagesV2(input2Messages, input2RulesV2))
+        assertEquals(306, countValidMessagesV2(inputMessages, inputRulesV2))
     }
 }
