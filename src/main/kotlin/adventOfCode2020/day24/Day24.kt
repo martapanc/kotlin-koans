00dc27d1e24e30679efb162a166b6a107b10717c
Marta Pancaldi <marta.panc@gmail.com>
2020-12-24 11:36:33 +0000

    AoC2020 D24P1: complete

--- src/main/kotlin/adventOfCode2020/day24/Day24.kt
+++ src/main/kotlin/adventOfCode2020/day24/Day24.kt
@@ -1,5 +1,6 @@
 package adventOfCode2020.day24
 
+import adventOfCode2020.day20.Coord
 import java.io.File
 
 fun readInputToList(path: String): List<List<HexNeighbors>> {
@@ -14,7 +15,7 @@ fun readInputToList(path: String): List<List<HexNeighbors>> {
                 hexNeighbors.add(HexNeighbors.valueOf(line[i].toString().toUpperCase()))
                 i++
             } else {
-                hexNeighbors.add(HexNeighbors.valueOf(line.substring(i, i+2).toUpperCase()))
+                hexNeighbors.add(HexNeighbors.valueOf(line.substring(i, i + 2).toUpperCase()))
                 i += 2
             }
         }
@@ -23,4 +24,43 @@ fun readInputToList(path: String): List<List<HexNeighbors>> {
     return inputList
 }
 
+fun findTilesAndCountBlackOnes(input: List<List<HexNeighbors>>): Int {
+    val tilesMap = mutableMapOf<Coord, TileColor>()
+    tilesMap[Coord(0, 0)] = TileColor.WHITE
+
+    val deltaMap = getDeltaMap()
+    for (instructionList in input) {
+        var currentTile = Coord(0, 0)
+        for (instruction in instructionList) {
+            val delta = deltaMap[instruction]
+            val nextTileCoord = Coord(currentTile.x + delta!!.x, currentTile.y + delta.y)
+            if (!tilesMap.containsKey(nextTileCoord)) {
+                tilesMap[nextTileCoord] = TileColor.WHITE
+            }
+            currentTile = nextTileCoord
+        }
+
+        if (tilesMap.containsKey(currentTile)) {
+            tilesMap[currentTile] = if (tilesMap[currentTile] == TileColor.WHITE) TileColor.BLACK else TileColor.WHITE
+        } else {
+            tilesMap[currentTile] = TileColor.BLACK
+        }
+    }
+
+    return tilesMap.values.count { it == TileColor.BLACK }
+}
+
+private fun getDeltaMap(): MutableMap<HexNeighbors, Coord> {
+    val deltaMap = mutableMapOf<HexNeighbors, Coord>()
+    deltaMap[HexNeighbors.NW] = Coord(0, 1)
+    deltaMap[HexNeighbors.NE] = Coord(1, 1)
+    deltaMap[HexNeighbors.E] = Coord(1, 0)
+    deltaMap[HexNeighbors.SE] = Coord(0, -1)
+    deltaMap[HexNeighbors.SW] = Coord(-1, -1)
+    deltaMap[HexNeighbors.W] = Coord(-1, 0)
+    return deltaMap
+}
+
 enum class HexNeighbors { E, NE, SE, W, NW, SW }
+
+enum class TileColor { WHITE, BLACK }
--- src/test/kotlin/adventOfCode2020/day24/Day24KtTest.kt
+++ src/test/kotlin/adventOfCode2020/day24/Day24KtTest.kt
@@ -10,7 +10,14 @@ internal class Day24KtTest {
 
     @Test
     fun readInputToList() {
+        println(input0)
         assertEquals(20, input0.size)
         assertEquals(597, input.size)
+    }
+
+    @Test
+    fun findTiles() {
+        assertEquals(10, findTilesAndCountBlackOnes(input0))
+        assertEquals(549, findTilesAndCountBlackOnes(input))
     }
 }
