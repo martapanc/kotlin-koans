e1af8750c31af5ca54831d5e61f1b8c881e6c36f
Marta Pancaldi <marta.panc@gmail.com>
2020-12-25 13:30:39 +0000

    AoC2020 D25: complete

--- src/main/kotlin/adventOfCode2020/day25/Day25.kt
+++ src/main/kotlin/adventOfCode2020/day25/Day25.kt
@@ -1,4 +1,37 @@
 package adventOfCode2020.day25
 
-class Day25 {
+fun findEncryptionKey(doorPubKey: Int, cardPubKey: Int, subjectNum: Int = 7): Long {
+    val constant = 20201227
+    var doorLoopNum = 0
+    var cardLoopNum = 0
+
+    var doorComputedPubKey = 1
+    var cardComputedPubKey = 1
+
+    while (cardComputedPubKey != cardPubKey) {
+        cardComputedPubKey *= subjectNum
+        cardComputedPubKey %= constant
+        cardLoopNum++
+    }
+    while (doorComputedPubKey != doorPubKey) {
+        doorComputedPubKey *= subjectNum
+        doorComputedPubKey %= constant
+        doorLoopNum++
+    }
+
+    var computedEncryptionKey = 1L
+    for (i in 0 until cardLoopNum) {
+        computedEncryptionKey *= doorPubKey
+        computedEncryptionKey %= constant
+    }
+
+    var computerEncryptionKeyCheck = 1L
+    for (i in 0 until doorLoopNum) {
+        computerEncryptionKeyCheck *= cardPubKey
+        computerEncryptionKeyCheck %= constant
+    }
+
+    if (computedEncryptionKey == computerEncryptionKeyCheck)
+        return computedEncryptionKey
+    return -1
 }
